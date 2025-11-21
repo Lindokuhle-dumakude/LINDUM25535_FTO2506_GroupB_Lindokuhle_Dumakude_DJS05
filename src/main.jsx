6b26e9bd4c190c8f6cab2d5c3c8477bb2ae58ec8
unknown <lindokuhle.dumakude11@gmail.com>
2025-11-21 13:19:39 +0200

    wrap App with BrowserRouter to enable routing

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { PodcastProvider } from "./context/PodcastContext.jsx";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <PodcastProvider>
-    <App />
-  </PodcastProvider>
+  <BrowserRouter>
+    <PodcastProvider>
+      <App />
+    </PodcastProvider>
+  </BrowserRouter>
 );
