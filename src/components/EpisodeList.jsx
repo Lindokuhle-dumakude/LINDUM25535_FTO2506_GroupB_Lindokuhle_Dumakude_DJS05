003231f5e0abd46c45bd3181d97a61703cdf66ab
unknown <lindokuhle.dumakude11@gmail.com>
2025-11-23 10:06:29 +0200

    add episode list with images, descriptions & titles

---
+++ src/components/EpisodeList.jsx
@@ -0,0 +1,28 @@
+import "../styles/EpisodeList.css";
+
+/**
+ * Displays a list of episodes for the selected season.
+ */
+export default function EpisodeList({ episodes }) {
+  return (
+    <div className="episode-list">
+      {episodes.map((ep) => (
+        <div key={ep.id} className="episode-card">
+          <img src={ep.image} alt={ep.title} className="episode-image" />
+
+          <div className="episode-content">
+            <h4 className="episode-title">
+              Episode {ep.episode}: {ep.title}
+            </h4>
+
+            <p className="episode-description">
+              {ep.description && ep.description.length > 150
+                ? ep.description.slice(0, 150) + "..."
+                : ep.description}
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
